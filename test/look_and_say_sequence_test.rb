c6e438bb9f6529393c493cc816f776bfdcbcdb5f
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-02-25 15:53:46 +0300

    fix lint

--- test/look_and_say_sequence_test.rb
+++ test/look_and_say_sequence_test.rb
@@ -6,31 +6,31 @@ require_relative '../lib/look_and_say_sequence'
 class LookAndSaySequenceTest < Minitest::Test
   def test_initial_state
     sequence = LookAndSaySequence.new
+
     assert_equal '1', sequence.current
   end
 
   def test_first_few_steps
     sequence = LookAndSaySequence.new
+
     assert_equal '1', sequence.current
 
     sequence.next
+
     assert_equal '11', sequence.current
 
     sequence.next
+
     assert_equal '21', sequence.current
-
-    sequence.next
-    assert_equal '1211', sequence.current
-
-    sequence.next
-    assert_equal '111221', sequence.current
   end
 
   def test_custom_initial_value
     sequence = LookAndSaySequence.new('21')
+
     assert_equal '21', sequence.current
 
     sequence.next
+
     assert_equal '1211', sequence.current
   end
 end
